3ca486c9fa0efa5ce367a77ed5a67c7debf32489
Piotr Nowosielski <piotrnowo@student.agh.edu.pl>
2025-06-01 00:36:00 +0200

    Deleted unnecessary input in main file

--- projekt_main.py
+++ projekt_main.py
@@ -9,19 +9,18 @@ from vtkmodules.util import numpy_support
 import pandas as pd
 from pathlib import Path
 
+# Tutaj potrzebujemy tylko posegmentowanych danych do walidacji modelu
+
 data_dir = Path('../DATA')
-raw_files = sorted([f for f in data_dir.glob('**/*.nrrd') if 'seg' not in f.name])
 seg_files = sorted([f for f in data_dir.glob('**/*seg*.nrrd')])
 
 def get_base_name(path):
     return path.stem.replace('.seg', '').replace('_seg', '')
 
-raw_dict = {get_base_name(f): f for f in raw_files}
 seg_dict = {get_base_name(f): f for f in seg_files}
 
-common_keys = set(raw_dict.keys()) & set(seg_dict.keys())
+common_keys = set(seg_dict.keys())
 df = pd.DataFrame({
-    'raw_path': [str(raw_dict[k]) for k in common_keys],
     'seg_path': [str(seg_dict[k]) for k in common_keys]
 })
 
